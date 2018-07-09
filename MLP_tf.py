28e0c0c0d8e236b71b09800b00f3965b0693e27f
Benjamin Amos <benjamin.oxi@gmail.com>
2018-07-09 02:34:47 -0400

    Arreglada la funcionalidad del training

--- MLP_tf.py
+++ MLP_tf.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
 import numpy as np
-import pandas as pd
-import sys
 
 
 def getData(file):
@@ -22,18 +20,20 @@ def main():
         "./Mark1 Data/Consumer Cyclical Sector/Consumer Cyclical Sector.test.30.txt")
 
     print("Defining tensorflow variables")
+
     # Se definen placeholders para las variables de datos en tensorflow
     x_ = tf.placeholder(tf.float32, [None, x.shape[1]])
     y_ = tf.placeholder(tf.int8, [None, 1])
+
     print("Defining parameters")
     # Parametros del funcionamiento de la red
-    LEARNING_RATE = 0.0005          # Tasa de aprendizaje
-    EPOCHS = 20                     # Numero de ciclos de feedforward - backpropagation
+    LEARNING_RATE = 0.001           # Tasa de aprendizaje
+    EPOCHS = 10                     # Numero de ciclos de feedforward - backpropagation
     # Numero de neuronas para input layer (tantas como filas en el archivo)
     N_INPUT = x.shape[0]
     N_HIDDEN_LAYERS = 2             # Numero de hidden layers
     # Por motivos de prueba, tendran la mitad de neuronas que input layer
-    HIDDEN_LAYER_SIZE = int(N_INPUT/2)
+    HIDDEN_LAYER_SIZE = 10          # int(N_INPUT/2)
     OUTPUT_LAYER_SIZE = 1           # Output layer tendra una sola neurona
     N_CLASSES = 1                   # Los posibles valores que puede tomar la tendencia
     print("Learning rate:", LEARNING_RATE, "\nEpochs:",
@@ -91,47 +91,65 @@ def main():
     def train_neural_network(x, y):
         print("Starting training..")
 
+        # Se convierten los arrays en tensors y se transpone *y* por consistencia con
+        # la prediccion pues es un arreglo plano y debe ser una matriz columna
         x_tf = tf.convert_to_tensor(x, np.float32)
         y = np.array([y], dtype=np.float32, ndmin=1)
         y = np.transpose(y)
-        
-        print(y.shape)
-        
         y_tf = tf.convert_to_tensor(np.transpose(y), np.float32)
-        
+
         print("Tensor x", x_tf, "Tensor y", y_tf)
         print("Getting the DNN model..")
-        
+
+        # Se obtiene la prediccion
         prediction = DeepNeuralNetwork(N_HIDDEN_LAYERS)
 
         print("Preparing cost function and optimizer")
-        
+
+        # Aca decidir entre usar como funcion de activacion cross entropy (hace backprop) a menos que
+        # se indique lo contrario) o sigmoid
+
         # cost = tf.reduce_mean(
-            # tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction, labels=y_tf))
+        #   tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction,labels=y_tf)
+        # )
         cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-            labels=tf.transpose(prediction), logits=y_tf)
-            )
+            logits=tf.transpose(prediction), labels=y_tf)
+        )
         optimizer = tf.train.GradientDescentOptimizer(
             LEARNING_RATE).minimize(cost)
-        
+
         print("Done.")
 
         print("Starting tensorflow session..")
-        
+
+        # Session es el entorno de ejecucion de tensorflow y aqui ocurre la ejecucion
         with tf.Session() as sess:
             print("Initializing variables..")
             sess.run(tf.global_variables_initializer())
 
             print("Running epochs..")
-            
+
+            # Una epoca es FeedForward + BackPropagation
             for epoch in range(EPOCHS):
                 epoch_loss = 0
+
+                # _ es una variable que no me interesa
                 for _ in range(x.shape[0]):
                     _, c = sess.run([optimizer, cost],
                                     feed_dict={x_: x, y_: y})
+                    print(c)
                     epoch_loss += c
-                
-                print("Epoch", epoch, "completed out of", EPOCHS, "loss")
+
+                print("Epoch", epoch, "completed out of",
+                      EPOCHS, "loss:", epoch_loss)
+
+
+        # Se muestra la precision del modelo
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+        # Aqui interesa 
+        # print("Accuracy:", accuracy.eval(x_: matriz de prueba, y_: vector clase de prueba))
 
     train_neural_network(x, y)
 
