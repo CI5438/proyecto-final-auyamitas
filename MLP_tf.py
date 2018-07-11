b787f5d7abab4005fc543e64b9ae0d27fb71e3b5
Benjamin Amos <benjamin.oxi@gmail.com>
2018-07-10 21:11:52 -0400

    Refactorizado como clase DeepNeuralNetwork

--- MLP_tf.py
+++ MLP_tf.py
@@ -1,77 +1,71 @@
 import tensorflow as tf
 import numpy as np
+import json
 
+class DeepNeuralNetwork():
+    def __init__(self, file, n_hidden):
+        self.file = file
+        self.x, self.y = self.getData(self.file)
 
-def getData(file):
-    print("Getting data from", file)
-    data = np.loadtxt(file, delimiter=";")
-    x = data[:, :-1]
-    y = data[:, -1]
+        self.LEARNING_RATE = 0.001
+        self.EPOCHS = 10
+        self.N_INPUT = self.x.shape[0]
+        self.N_HIDDEN_LAYERS = 2
+        self.HIDDEN_LAYER_SIZE = 15
+        self.N_CLASSES = 1
 
-    print("Retrieved data. Sliced features in a matrix with",
-          x.shape[0], "rows and", x.shape[1], "columns")
-    return x, y
+        self.layers = []
+        self.x_ = tf.placeholder(tf.float32, [None, self.x.shape[1]])
+        self.y_ = tf.placeholder(tf.int8, [None, 1])
 
+    def getData(self, file):
+        print("Getting data from", file)
+        data = np.loadtxt(file, delimiter=";")
+        x = data[:, :-1]
+        y = data[:, -1]
 
-def main():
-    print("Starting execution")
-    # Se obtiene la data procesada en np.arrays
-    x, y = getData(
-        "./Mark1 Data/Consumer Cyclical Sector/Consumer Cyclical Sector.test.30.txt")
+        print("Retrieved data. Sliced features in a matrix with",
+              x.shape[0], "rows and", x.shape[1], "columns")
+        return x, y
 
-    print("Defining tensorflow variables")
+    def setDNNModel(self):
+        actLayerSize = self.x.shape[1]
 
-    # Se definen placeholders para las variables de datos en tensorflow
-    x_ = tf.placeholder(tf.float32, [None, x.shape[1]])
-    y_ = tf.placeholder(tf.int8, [None, 1])
-
-    print("Defining parameters")
-    # Parametros del funcionamiento de la red
-    LEARNING_RATE = 0.001           # Tasa de aprendizaje
-    EPOCHS = 10                     # Numero de ciclos de feedforward - backpropagation
-    # Numero de neuronas para input layer (tantas como filas en el archivo)
-    N_INPUT = x.shape[0]
-    N_HIDDEN_LAYERS = 2             # Numero de hidden layers
-    # Por motivos de prueba, tendran la mitad de neuronas que input layer
-    HIDDEN_LAYER_SIZE = 10          # int(N_INPUT/2)
-    OUTPUT_LAYER_SIZE = 1           # Output layer tendra una sola neurona
-    N_CLASSES = 1                   # Los posibles valores que puede tomar la tendencia
-    print("Learning rate:", LEARNING_RATE, "\nEpochs:",
-          EPOCHS, "\nInput layer neurons:", N_INPUT, "\nHidden layers:",
-          N_HIDDEN_LAYERS, "\nHidden layers size:", HIDDEN_LAYER_SIZE,
-          "\nNumber of classes:", N_CLASSES)
-
-    def DeepNeuralNetwork(n_hidden_layers):
-        print("Starting DNN model build..")
-        hidden_layers = []
-        actLayerSize = x.shape[1]
-
-        print("Setting hidden layers weights and biases..")
         # Se definen las hidden layers que tendra la red
-        for i in range(0, n_hidden_layers):
+        for i in range(0, self.N_HIDDEN_LAYERS):
             hidden_layer_i = {
-                "weights": tf.Variable(tf.random_normal([actLayerSize, HIDDEN_LAYER_SIZE]), dtype=tf.float32),
-                "biases": tf.Variable(tf.random_normal([HIDDEN_LAYER_SIZE]), dtype=tf.float32),
+                "weights": tf.Variable(tf.random_normal(
+                        [actLayerSize, self.HIDDEN_LAYER_SIZE]), 
+                        dtype=tf.float32
+                    ),
+                "biases": tf.Variable(
+                    tf.random_normal([self.HIDDEN_LAYER_SIZE]),
+                     dtype=tf.float32
+                    ),
                 "name": "hidden_layer_" + str(i)
             }
-            actLayerSize = HIDDEN_LAYER_SIZE
-            hidden_layers.append(hidden_layer_i)
-        print("Done.")
-        print("Setting output layer weights and biases..")
+            actLayerSize = self.HIDDEN_LAYER_SIZE
+            self.layers.append(hidden_layer_i)
+        
         # Se define la output layer
         output_layer = {
-            "weights": tf.Variable(tf.random_normal([HIDDEN_LAYER_SIZE, N_CLASSES]), dtype=tf.float32),
-            "biases": tf.Variable(tf.random_normal([N_CLASSES]), dtype=tf.float32),
+            "weights": tf.Variable(
+                    tf.random_normal([self.HIDDEN_LAYER_SIZE, self.N_CLASSES]),
+                    dtype=tf.float32
+                ),
+            "biases": tf.Variable(
+                tf.random_normal([self.N_CLASSES]),
+                dtype=tf.float32
+                ),
             "name": "output_layer"
         }
-        print('Done.')
-        hidden_layers.append(output_layer)
+        self.layers.append(output_layer)
 
         # Se calcula el output a traves del feed forward
-        x_tf = tf.convert_to_tensor(x, np.float32)
+        x_tf = tf.convert_to_tensor(self.x, np.float32)
         actLayer = x_tf
-        print("Preparing output for prediction..")
-        for layer in hidden_layers:
+
+        for layer in self.layers:
             # En caso de que sean hidden layers tomamos como referencia la capa anterior
             # y aplicamos la funcion de activacion
             if layer["name"] != "output_layer":
@@ -83,28 +77,18 @@ def main():
             else:
                 output = tf.nn.xw_plus_b(li, layer["weights"], layer["biases"])
 
-        print("Output ready.")
-        print("Tensor DNN output:", output)
-        print("DNN model built.")
         return output
 
-    def train_neural_network(x, y):
-        print("Starting training..")
-
+    def trainDNN(self):
         # Se convierten los arrays en tensors y se transpone *y* por consistencia con
         # la prediccion pues es un arreglo plano y debe ser una matriz columna
-        x_tf = tf.convert_to_tensor(x, np.float32)
-        y = np.array([y], dtype=np.float32, ndmin=1)
-        y = np.transpose(y)
-        y_tf = tf.convert_to_tensor(np.transpose(y), np.float32)
-
-        print("Tensor x", x_tf, "Tensor y", y_tf)
-        print("Getting the DNN model..")
+        x_tf = tf.convert_to_tensor(self.x, np.float32)
+        self.y = np.array([self.y], dtype=np.float32, ndmin=1)
+        self.y = np.transpose(self.y)
+        y_tf = tf.convert_to_tensor(np.transpose(self.y), np.float32)
 
         # Se obtiene la prediccion
-        prediction = DeepNeuralNetwork(N_HIDDEN_LAYERS)
-
-        print("Preparing cost function and optimizer")
+        prediction = self.setDNNModel()
 
         # Aca decidir entre usar como funcion de activacion cross entropy (hace backprop) a menos que
         # se indique lo contrario) o sigmoid
@@ -116,45 +100,46 @@ def main():
             logits=tf.transpose(prediction), labels=y_tf)
         )
         optimizer = tf.train.GradientDescentOptimizer(
-            LEARNING_RATE).minimize(cost)
-
-        print("Done.")
-
-        print("Starting tensorflow session..")
+            self.LEARNING_RATE).minimize(cost)
 
         # Session es el entorno de ejecucion de tensorflow y aqui ocurre la ejecucion
         with tf.Session() as sess:
-            print("Initializing variables..")
             sess.run(tf.global_variables_initializer())
 
-            print("Running epochs..")
-
             # Una epoca es FeedForward + BackPropagation
-            for epoch in range(EPOCHS):
+            for epoch in range(self.EPOCHS):
                 epoch_loss = 0
 
                 # _ es una variable que no me interesa
-                for _ in range(x.shape[0]):
+                for _ in range(self.x.shape[0]):
                     _, c = sess.run([optimizer, cost],
-                                    feed_dict={x_: x, y_: y})
-                    print(c)
+                                    feed_dict={self.x_: self.x, self.y_: self.y})
+                    # print(c)
                     epoch_loss += c
 
                 print("Epoch", epoch, "completed out of",
-                      EPOCHS, "loss:", epoch_loss)
-
+                      self.EPOCHS, "loss:", epoch_loss)
 
         # Se muestra la precision del modelo
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(self.y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
-        # Aqui interesa 
+        # Aqui interesa
         # print("Accuracy:", accuracy.eval(x_: matriz de prueba, y_: vector clase de prueba))
 
-    train_neural_network(x, y)
+    def exportLayersAsJSON(self):
+        sectorName = self.file.split('/')[1]
+        json.dump(self.layers, sectorName + ".DNNweights.json")
 
-    # DNN = DeepNeuralNetwork(N_HIDDEN_LAYERS)
+    def importLayersFromJSON(self, fileName):
+        with open(fileName) as f:
+            self.layers = json.load(f)
 
+def main():
+    file = 'Mark1 Data/Consumer Cyclical Sector/Consumer Cyclical Sector.test.60.txt'
+    n_hidden_layers = 2
+    deepNeuralNetwork = DeepNeuralNetwork(file, n_hidden_layers)
+    deepNeuralNetwork.trainDNN()
 
 if __name__ == '__main__':
     main()
