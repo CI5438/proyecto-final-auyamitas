be5c6013e6fe5d53fa99de32ccd45322436931bc
lautarovc <casper1994@gmail.com>
2018-07-11 15:19:09 -0400

    Factoriza y agrega librerias de DNN

---
+++ dataLib.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+def getData(file):
+    print("Getting data from", file)
+    data = np.loadtxt(file, delimiter=";")
+    x = data[:, :-1]
+    y = data[:, -1]
+
+    print("Retrieved data. Sliced features in a matrix with",
+          x.shape[0], "rows and", x.shape[1], "columns")
+    return x, y
+
+# Lectura de datos CSV
+def readData(file, sep):
+    dataSet = pd.read_csv(file, sep=sep, comment="#", header=None)
+    return dataSet
+
+# Division de datos en x normalizado, x, y
+def prepareData(df):
+	x = df.drop(df.columns[[2]], axis=1)
+	x = x.values
+	lenx = len(x)
+	xNorm = x/np.amax(x, axis=0)
+
+	xNorm = np.c_[np.ones(lenx), xNorm]
+
+	y = df[[2]]
+	y = y.values
+
+	return xNorm, x, y
