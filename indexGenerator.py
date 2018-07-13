96cc959acd643e51e83d8bef863634b58f686bc4
Benjamin Amos <benjamin.oxi@gmail.com>
2018-07-13 11:23:15 -0400

    Modificado Index y testCase para generar archivos con precio mas alto y precio mas bajo si se desea

--- indexGenerator.py
+++ indexGenerator.py
@@ -1,68 +1,95 @@
-import os, sys
+import os
+import sys
 import pandas as pd
 import datetime
 
-def getIndex(sector):
 
-	# Ingresamos a la carpeta del sector
-	os.chdir("Mark1 Data/"+sector)
-	
-	# Guardamos los datasets
-	sets = []
-	fileList = os.listdir()
-	maxSet = []
-	for file in fileList:
-		if "Index" in file:
-			continue
+def getIndex(sector, addPrices):
 
-		dataSet = pd.read_csv(file, sep=";", header=None)
-		dataSet = dataSet.set_index([0])
+    # Ingresamos a la carpeta del sector
+    os.chdir("Mark1 Data/"+sector)
 
-		#Tomamos los indices del dataset mas grande
-		rowIndexes = dataSet.index.values
-		if len(maxSet) < len(rowIndexes):
-			maxSet = rowIndexes
+    # Guardamos los datasets
+    sets = []
+    fileList = os.listdir()
+    maxSet = []
+    for file in fileList:
+        if "Index" in file:
+            continue
 
-		sets.append(dataSet)
+        dataSet = pd.read_csv(file, sep=";", header=None)
+        dataSet = dataSet.set_index([0])
 
-	# Abrimos archivo de indice
-	fIndex = open(sector+" - Index.txt", "w")
+        # Tomamos los indices del dataset mas grande
+        rowIndexes = dataSet.index.values
+        if len(maxSet) < len(rowIndexes):
+            maxSet = rowIndexes
 
-	for date in maxSet:
+        sets.append(dataSet)
 
-		openPrice = 0
-		closePrice = 0
-		numSets = 0
-		for dataSet in sets:
-			# Obtenemos la fila de la fecha actual
-			try:
-				row = dataSet.loc[date]
-				numSets += 1
-			# Pasamos al siguiente set si no existe la fila
-			except:
-				continue
+    # Abrimos archivo de indice
+    fIndex = open(sector+" - Index.txt", "w")
 
-			openPrice += row[1]
-			closePrice += row[4]
+    for date in maxSet:
 
-		# Promediamos
-		openPrice = round(openPrice / numSets, 3)
-		closePrice = round(closePrice / numSets, 3)
+        openPrice = 0
+        closePrice = 0
 
-		# Escribimos en el archivo
-		fIndex.write(str(date)+";"+str(openPrice)+";"+str(closePrice)+"\n")
+        if addPrices:
+            lowerPrice = 0
+            maxPrice = 0
 
+        numSets = 0
+        for dataSet in sets:
+            # Obtenemos la fila de la fecha actual
+            try:
+                row = dataSet.loc[date]
+                numSets += 1
+            # Pasamos al siguiente set si no existe la fila
+            except:
+                continue
 
-	fIndex.close()
+            openPrice += row[1]
+            closePrice += row[4]
+            if addPrices:
+                lowerPrice += row[2]
+                maxPrice += row[3]
+
+        # Promediamos
+        openPrice = round(openPrice / numSets, 3)
+        closePrice = round(closePrice / numSets, 3)
+
+        if addPrices:
+            lowerPrice = round(lowerPrice / numSets, 3)
+            maxPrice = round(maxPrice / numSets, 3)
+
+        # Escribimos en el archivo
+        if not addPrices:
+            fIndex.write(str(date)+";"+str(openPrice)+";"+str(closePrice)+"\n")
+        else:
+            fIndex.write(str(date)+";"+str(openPrice)+";" +
+                         str(closePrice)+";"+str(lowerPrice)+";"+str(maxPrice)+"\n")
+
+    fIndex.close()
+
 
 def main():
-	try:
-		sector = sys.argv[1]
-	except:
-		print("Ingrese el Sector a Producir el Indice:")
-		sector = input()
+    try:
+        sector = sys.argv[1]
+    except:
+        print("Ingrese el Sector a Producir el Indice:")
+        sector = input()
 
-	getIndex(sector)
+    try:
+        print("Ingrese 1 si desea incluir en el indice el precio mas alto y el mas bajo diario, de lo contrario ingrese 0:")
+        addMaxMinPrices = int(input())
+        assert(addMaxMinPrices == 1 or addMaxMinPrices == 0)
+    except:
+        print("Error, ha ingresado un valor incorrecto.")
+        sys.exit()
+
+    getIndex(sector, addMaxMinPrices)
+
 
 if __name__ == '__main__':
-	main()
+    main()
